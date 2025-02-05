9bf30bdeb0692675de50ac7d7d5810c75e7154bd
Quinlan Bush <quinnbush5@gmail.com>
2025-02-05 12:39:32 -0800

    pick up items fixed

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class InventorySystem : MonoBehaviour
 {
     public static InventorySystem Instance { get; set; }
-
     public GameObject inventoryScreenUI;
+
+    public List<GameObject> slotList = new List<GameObject>();
+    public List<string> itemList = new List<string>();
+    private GameObject itemToAdd;
+    private GameObject whatSlotToEquip;
+
     public bool isOpen;
+
+    // public bool isFull;
 
     private void Awake()
     {
@@ -25,6 +32,20 @@ public class InventorySystem : MonoBehaviour
     void Start()
     {
         isOpen = false;
+        // isFull = false;
+
+        PopulateSlotList();
+    }
+
+    private void PopulateSlotList()
+    {
+        foreach (Transform child in inventoryScreenUI.transform)
+        {
+            if (child.CompareTag("Slot"))
+            {
+                slotList.Add(child.gameObject);
+            }
+        }
     }
 
     void Update()
@@ -41,6 +62,51 @@ public class InventorySystem : MonoBehaviour
             inventoryScreenUI.SetActive(false);
             Cursor.lockState = CursorLockMode.Locked;
             isOpen = false;
+        }
+    }
+
+    public void AddToInventory(string itemName)
+    {
+        whatSlotToEquip = FindNextEmptySlot();
+        itemToAdd = Instantiate(
+            Resources.Load<GameObject>(itemName),
+            whatSlotToEquip.transform.position,
+            whatSlotToEquip.transform.rotation
+        );
+        itemToAdd.transform.SetParent(whatSlotToEquip.transform);
+        itemList.Add(itemName);
+    }
+
+    private GameObject FindNextEmptySlot()
+    {
+        foreach (GameObject slot in slotList)
+        {
+            if (slot.transform.childCount == 0)
+            {
+                return slot;
+            }
+        }
+        return new GameObject();
+    }
+
+    public bool CheckIfFull()
+    {
+        int counter = 0;
+
+        foreach (GameObject slot in slotList)
+        {
+            if (slot.transform.childCount > 0)
+            {
+                counter += 1;
+            }
+        }
+        if (counter == 21)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
